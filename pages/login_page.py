98267a7905a077c44d6211acbb013696b5d55eb3
Nishara1022 <tharushinishara1022@gmail.com>
2025-06-26 03:46:00 +0530

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -7,12 +7,14 @@ class Login():
         self.driver = driver
 
 
+    # locators
     LOGIN_EMAIL_INPUT = (By.XPATH, "//input[@data-qa='login-email']")
     LOGIN_PASSWORD_INPUT = (By.XPATH, "//input[@data-qa='login-password']")
     LOGIN_BUTTON = (By.XPATH, "//button[@data-qa='login-button']")
 
     
     print("---- Login page ----")
+    
     # login form
     def login(self, email, password):
         print("-- Login Form --")
