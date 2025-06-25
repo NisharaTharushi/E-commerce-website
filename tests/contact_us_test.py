7da98853f882d69c384c38749e3fc862ff9f04a6
Nishara1022 <tharushinishara1022@gmail.com>
2025-06-26 03:51:06 +0530

    Update contact_us_test.py

--- tests/contact_us_test.py
+++ tests/contact_us_test.py
@@ -3,17 +3,12 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from selenium import webdriver
-
-
 from pages.contact_us_page import ContactUsPage
 
 driver = webdriver.Firefox() 
 driver.get("https://www.automationexercise.com/contact_us")
-# maximize_window
-
 
 contact = ContactUsPage(driver)
-
 
 contact.set_name("Tharushi")
 contact.set_email("tharushina@gmail")
